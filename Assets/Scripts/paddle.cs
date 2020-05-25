3232df2081101980b3b6682f7e6f6949048ae808
Daniel Han <danielhan98@gmail.com>
2020-05-25 10:29:51 -0400

    layout, paddles, and paddle scripts

---
+++ Assets/Scripts/paddle.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class paddle : MonoBehaviour
+{
+
+    public bool isPlayer1;
+    public float speed;
+    public Rigidbody2D rb;
+
+    private float movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //changes player's method of movement, sw if P1 up and down if P2
+        if (isPlayer1)
+        {
+            movement = Input.GetAxisRaw("Vertical");
+        }
+        else
+        {
+            movement = Input.GetAxisRaw("Vertical2");
+        }
+
+        rb.velocity = new Vector2(0, movement * speed);
+    }
+}
